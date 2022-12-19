5091c9563261f998591ad7f2ac56045bb01d2a66
Okwoli-Godwin <okwolig60@gmail.com>
2022-12-19 17:55:39 +0100

    added

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose"
 
 const URI: string = "mongodb://localhost/bookstore"
+const liveURI: string = "mongodb+srv://Godwin:mango@cluster0.8usbdhw.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect(URI)
+mongoose.connect(liveURI)
 mongoose.connection
 .on("open", () => {
     console.log("Database connected")
